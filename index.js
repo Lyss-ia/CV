06a063a2f07f72b7a3e24333ba2b6eb095929249
Lyss-ia <morgane.vb@orange.fr>
2022-05-15 20:20:50 +0200

    CV v1 fini

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
+//Variables
 var i=0;
 var j=0;
-var clair="rgb(222, 172, 240)";
+var clair="rgb(222, 172, 240)"; 
 var moyen="rgb(139, 89, 158)";
 var fonce="rgb(47, 36, 46)";
 var fonce2="rgb(24, 19, 24)";
+
+//Agrandir l'image de profil
 
 $("img").on("click", function(){
     if(i==0){
         console.log("click");
         $("img").addClass("click").css("transition", "width, 2s");
-        $("aside").css("height",100+"px");
+        $("aside").css("height",150+"px");
         i=1;
 
     }
     else{
         
         $("img").removeClass("click").css("transition", "width 2s");
-        $("aside").css("height","auto");
+        // $("aside").css("height","auto");
         i=0;
     }
     
@@ -24,6 +27,7 @@ $("img").on("click", function(){
 
 });
 
+//Mode sombre 
 
 $("button").on("click",function(){
 
@@ -32,7 +36,7 @@ $("button").on("click",function(){
         $("body").css("background-color", moyen).css("color", clair);
         $("header,main").css("background-color",fonce);
         $("button").css("background-color",clair).css("color",fonce).html("☉");
-        $("a").css("color",clair);
+        $("a,svg").css("color",clair);
 
         j=1;
 
@@ -42,7 +46,7 @@ $("button").on("click",function(){
         $("body").css("background-color", clair).css("color",fonce2);
         $("header,main").css("background-color",moyen);
         $("button").css("background-color",fonce).css("color",clair).html("☽");
-        $("a").css("color",fonce)
+        $("a,svg").css("color",fonce)
 
         j=0;
     }
