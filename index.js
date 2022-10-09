d7df27ba42cb9292c099c49c3ba72c0e6fce2a32
Lyss-ia <morgane.vb@orange.fr>
2022-10-09 16:03:01 +0200

    new mod

--- index.js
+++ index.js
@@ -2,20 +2,20 @@
 
 let i=0;
 let j=0;
-const clair="rgb(222, 172, 240)"; 
-const moyen="rgb(139, 89, 158)";
-const fonce="rgb(47, 36, 46)";
-const fonce2="rgb(24, 19, 24)";
-const lila = "rgb(197,163,230)";
+const light="rgb(222, 172, 240)"; 
+const medium="rgb(139, 89, 158)";
+const dark="rgb(47, 36, 46)";
+const darker="rgb(24, 19, 24)";
+const lilac = "rgb(197,163,230)";
 const image = document.querySelector("img");
 const note = document.querySelector("aside");
-const bouton = document.querySelector("button");
-const corps = document.querySelector("body");
-const entete = document.querySelectorAll("header, main");
-const lien = document.querySelectorAll("a, svg");
+const button = document.querySelector("button");
+const body = document.querySelector("body");
+const head = document.querySelectorAll("header, main");
+const link = document.querySelectorAll("a, svg");
       
 
-if(localStorage.getItem("theme")=="clair"){
+if(localStorage.getItem("theme")=="day"){
     j=1;
 }
 else{
@@ -44,28 +44,28 @@ image.onclick = function(){
 
 //Mode sombre 
 
-const changertheme = function(){
+const changetheme = function(){
 
     if(j==0){
 
-        corps.style.backgroundColor = moyen;
-        corps.style.color = clair;
-        note.style.color = fonce;
-        note.style.outlineColor = clair;
-        bouton.style.backgroundColor = clair;
-        bouton.style.color = fonce;
-        bouton.innerText = "☉" ;
-        localStorage.setItem("theme", "sombre");
+        body.style.backgroundColor = medium;
+        body.style.color = light;
+        note.style.color = dark;
+        note.style.outlineColor = light;
+        button.style.backgroundColor = light;
+        button.style.color = dark;
+        button.innerText = "☉" ;
+        localStorage.setItem("theme", "night");
         
-        for (const iterateur of entete) {
+        for (const iterator of head) {
 
-            iterateur.style.backgroundColor = fonce;
+            iterator.style.backgroundColor = dark;
             
         }
 
-        for (const iterateur of lien) {
+        for (const iterator of link) {
 
-            iterateur.style.color = clair;
+            iterator.style.color = light;
         }
 
         j=1;
@@ -74,22 +74,22 @@ const changertheme = function(){
 
     else{
         
-        corps.style.backgroundColor = clair;
-        corps.style.color = fonce2;
-        note.style.outlineColor = fonce2;
-        bouton.style.backgroundColor = fonce2;
-        bouton.style.color = lila;
-        bouton.innerText = "☽";
-        localStorage.setItem("theme", "clair");
+        body.style.backgroundColor = light;
+        body.style.color = darker;
+        note.style.outlineColor = darker;
+        button.style.backgroundColor = darker;
+        button.style.color = lilac;
+        button.innerText = "☽";
+        localStorage.setItem("theme", "day");
 
-        for (const iterateur of entete) {
+        for (const iterator of head) {
 
-            iterateur.style.backgroundColor = lila;
+            iterator.style.backgroundColor = lilac;
         }
 
-        for (const iterateur of lien) {
+        for (const iterator of link) {
 
-            iterateur.style.color = fonce2;
+            iterator.style.color = darker;
             
         }
 
@@ -98,14 +98,14 @@ const changertheme = function(){
     
 };
 
-bouton.onclick = changertheme;
-changertheme();
+button.onclick = changetheme;
+changetheme();
 
 // Date d'anniversaire
 
-function calculerAge(anniv = "2000-10-31") {
-    const annif = new Date(anniv);
-    return Math.abs(new Date(Date.now() - annif.getTime()).getUTCFullYear() - 1970);
+function calculateAge(anniv = "2000-10-31") {
+    const anniversary = new Date(anniv);
+    return Math.abs(new Date(Date.now() - anniversary.getTime()).getUTCFullYear() - 1970);
 };
 
-let bulle = document.querySelector(".bubulle").setAttribute("aria-label", calculerAge() + " ans");
+let bubble = document.querySelector(".bubble").setAttribute("aria-label", calculateAge() + " ans");
